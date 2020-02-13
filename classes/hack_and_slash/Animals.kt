d4a49457be3340a3bce6b0f1bcbed8dc88c1267c
t0in4 <t0in4@yahoo.com>
2020-02-13 22:54:02 +0900

    new

---
+++ classes/hack_and_slash/Animals.kt
@@ -0,0 +1,45 @@
+open class Animal {
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    val hunger = 10
+    open fun makeNoise(){
+        println("The Anima is making a noise")
+    }
+    open fun eat() {
+        println("The Animal is eating")
+    }
+    open fun roam(){
+        println("The Animal is roaming")
+    fun sleep(){
+        println("The Animal is sleeping")
+    }
+}
+}
+class Hippo : Animal() {
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+open class Canine: Animal() {
+    override fun roam() {
+        println("The Canine is roaming")
+    }
+}
+class Wolf: Canine() {
+    override val image = "wolf.jpg"
+    override val food = "meat"
+    override val habitat = "forests"
+    override fun makeNoise(){
+        println("Hooooowl!")
+    }
+    override fun eat(){
+        println("The Wolf is eating $food")
+    }
+}
