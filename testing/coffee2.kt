daf14e34e645991b4a2ec2ce9c62a39484001b60
t0in4 <t0in4@yahoo.com>
2020-02-20 14:40:00 +0900

    testing

--- testing/coffee2.kt
+++ testing/coffee2.kt
@@ -11,12 +11,11 @@ fun main() {
     print("Write how many cups of coffee you will need: ")
     val cups = scanner.nextInt()
     if (water/(cups * 200) != 0 && milk/(cups * 50) != 0 && beans/(cups * 15) != 0) {
-        print("Yes, I can make that amount of coffee")}
-        else if (water / (cups * 200) != cups ) {
-            if (beans/(cups*15)>(((beans/15)*cups)-cups)&&((milk/(cups*50)>((milk/50)*cups)-cups))){
+        println("Yes, I can make that amount of coffee")}
+            else if (water/(cups*200)>(((water/200)*cups)-cups)&&beans/(cups*15)>(((beans/15)*cups)-cups)&&((milk/(cups*50)>((milk/50)*cups)-cups))){
             
-            print("(and even ${water / (cups * 200)} more than that)")
-        }} else (print("No, I can make only ${(water/(cups*200))} cups of coffee"))
+            print("(and even ${water / (cups * 200)-cups} more than that)")
+        } else (print("No, I can make only ${(water/(cups*200))} cups of coffee"))
 
 }
 
